b10c5456b715e3c059df1d0aa76de4b851942e0c
BJSwamy <annishh215@gmail.com>
2020-06-12 16:07:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase3/user.java
@@ -0,0 +1,28 @@
+package com.example.firebase3;
+
+public class user {
+    String id;
+    String name;
+    String cals;
+    public user() {
+
+    }
+
+    public user(String id, String name, String cals) {
+        this.id = id;
+        this.name = name;
+        this.cals = cals;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCals() {
+        return cals;
+    }
+}
